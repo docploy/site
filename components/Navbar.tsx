be5823d1c183b1882022d080264fbf4211cb5009
Charlie <cvuong90@gmail.com>
2022-07-27 16:12:09 -0700

    Add a link to the blog section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,22 @@
 import { GITHUB_ACTION_URL } from 'constants/url';
+import Link from 'next/link';
 
 function Navbar() {
   return (
     <nav className="align-baseline flex justify-between">
-      <span className="font-bold text-2xl">Docploy</span>
-      <a href={GITHUB_ACTION_URL}>
-        <button className="bg-amber-400 font-bold hover:bg-amber-300 py-2 px-4 rounded text-gray-900">
-          Install on GitHub
-        </button>
-      </a>
+      <Link href="/">
+        <span className="cursor-pointer font-bold text-2xl">Docploy</span>
+      </Link>
+      <div className="flex gap-4 items-center">
+        <Link href="/blog">
+          <a className="font-bold">Blog</a>
+        </Link>
+        <a href={GITHUB_ACTION_URL}>
+          <button className="bg-amber-400 font-bold hover:bg-amber-300 py-2 px-4 rounded text-gray-900">
+            Install on GitHub
+          </button>
+        </a>
+      </div>
     </nav>
   );
 }
