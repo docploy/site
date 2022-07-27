897f6b05c102bb0db58f9f54409ca2868c8ab8c9
Charlie <cvuong90@gmail.com>
2022-07-27 16:13:36 -0700

    Make the shell contain the header, main, and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Navbar from 'components/Navbar';
 import PlausibleProvider from 'next-plausible';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlausibleProvider domain="docploy.com" trackOutboundLinks>
-      <Component {...pageProps} />;
+      <div className="m-8">
+        <div className="m-auto max-w-7xl">
+          <nav>
+            <Navbar />
+          </nav>
+          <main className="m-auto max-w-4xl my-12">
+            <Component {...pageProps} />
+          </main>
+          <footer className="text-center text-gray-500">
+            <h1>© Docploy</h1>
+          </footer>
+        </div>
+      </div>
     </PlausibleProvider>
   );
 }
