f47cf3a7638096c9749c8bbb962e0c0caa4a0703
Charlie <cvuong90@gmail.com>
2022-08-03 19:44:50 -0700

    Wrap amin component with UserProvider and AlertProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,34 @@
 import '../styles/globals.css';
 
+import Alert from 'components/Alert';
+import AlertProvider from 'contexts/alert';
 import type { AppProps } from 'next/app';
 import Navbar from 'components/Navbar';
 import PlausibleProvider from 'next-plausible';
+import UserProvider from 'contexts/user';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PlausibleProvider domain="docploy.com" trackOutboundLinks>
-      <div className="m-8">
-        <div className="m-auto max-w-7xl">
-          <nav>
-            <Navbar />
-          </nav>
-          <main className="m-auto max-w-4xl my-12">
-            <Component {...pageProps} />
-          </main>
-          <footer className="text-center text-gray-500">
-            <h1>© Docploy</h1>
-          </footer>
-        </div>
-      </div>
-    </PlausibleProvider>
+    <UserProvider>
+      <PlausibleProvider domain="docploy.com" trackOutboundLinks>
+        <AlertProvider>
+          <Alert />
+          <div className="m-8">
+            <div className="m-auto max-w-7xl">
+              <nav>
+                <Navbar />
+              </nav>
+              <main className="m-auto max-w-4xl my-12">
+                <Component {...pageProps} />
+              </main>
+              <footer className="text-center text-gray-500">
+                <p>© Docploy</p>
+              </footer>
+            </div>
+          </div>
+        </AlertProvider>
+      </PlausibleProvider>
+    </UserProvider>
   );
 }
 
