f27914591227d2dc5b6abbfc82d4cd7bbcdff697
Charlie <cvuong90@gmail.com>
2022-08-05 10:03:11 -0700

    Refactor portal API endpoint to use Stripe

--- pages/api/portal.ts
+++ pages/api/portal.ts
@@ -1,40 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import Stripe from 'stripe';
-import cookie from 'cookie';
-import { supabase } from 'utils/supabase';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { user } = await supabase.auth.api.getUserByCookie(req);
-
-  if (!user) {
-    return res.status(401).send('Unauthorized');
-  }
-
-  const token = cookie.parse(req.headers.cookie || '')['sb-access-token'];
-  supabase.auth.session = (): any => ({
-    access_token: token,
-  });
-
   const {
-    data: { stripe_customer },
-  } = await supabase
-    .from('profile')
-    .select('stripe_customer')
-    .eq('id', user.id)
-    .single();
+    body: { session_id },
+  } = req;
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
     apiVersion: '2020-08-27',
   });
 
-  const session = await stripe.billingPortal.sessions.create({
-    customer: stripe_customer,
+  const checkoutSession = await stripe?.checkout.sessions.retrieve(session_id);
+  const customer = checkoutSession.customer as string;
+
+  const portalSession = await stripe?.billingPortal.sessions.create({
+    customer,
     return_url: process.env.NEXT_PUBLIC_BASE_URL,
   });
 
   res.send({
-    url: session.url,
+    url: portalSession.url,
   });
 };
 
