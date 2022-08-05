80f1fcb0d021e0665e6c954e00b1bfcd6454daad
Charlie <cvuong90@gmail.com>
2022-08-05 10:52:06 -0700

    Redirect the user after they log out

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,11 +1,14 @@
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { useUser } from 'contexts/user';
 
 function Logout() {
   const { logout } = useUser();
+  const router = useRouter();
 
   useEffect(() => {
     logout();
+    router.push('/');
   }, []);
 
   return <p>Logging out</p>;
