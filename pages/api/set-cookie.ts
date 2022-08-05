8c2c878fd2f24043c9b20991fb1de365fe89a58c
Charlie <cvuong90@gmail.com>
2022-08-05 10:26:36 -0700

    Remove unused API endpoints

--- pages/api/set-cookie.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-import { supabase } from 'utils/supabase';
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  await supabase.auth.api.setAuthCookie(req, res);
-};
-
-export default handler;
