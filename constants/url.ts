dfec888377e0f64902a4d90f08609d487e6413b8
Charlie <cvuong90@gmail.com>
2022-08-24 20:52:30 -0700

    Revise landing page with better graphics

--- constants/url.ts
+++ constants/url.ts
@@ -1,2 +1,5 @@
 export const GITHUB_ACTION_URL =
   'https://github.com/marketplace/actions/publish-docs-to-github-pages';
+
+export const GET_STARTED_URL =
+  'https://docploy.github.io/site/getting-started/installation';
